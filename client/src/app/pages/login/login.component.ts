8272bbbb951c89003f7cf6a0a387744067f1afe8
Freeshea <obenceoh@gmail.com>
2025-04-25 13:25:03 +0200

    Login styling

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -1,39 +1,63 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { Router, RouterModule } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink} from '@angular/router';
 import { AuthService } from '../../shared/services/auth.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatError, MatFormField, MatInput, MatLabel } from '@angular/material/input';
+import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterLink,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInput,
+    MatLabel,
+    MatError,
+    MatFormField,
+    MatCard,
+    MatCardHeader,
+    MatCardActions,
+    MatCardTitle,
+    MatCardContent
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  email: string = '';
-  password: string = '';
-  errorMessage: string = '';
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+  });
 
   constructor(private router: Router, private authService: AuthService) { }
 
+  get isFormValid(): boolean {
+    return this.loginForm.valid;
+  }
+
   login() {
-    if (this.email && this.password) {
-      this.errorMessage = '';
-      this.authService.login(this.email, this.password).subscribe({
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+    const { email, password } = this.loginForm.value;
+
+    if(email && password){
+      this.authService.login(email, password).subscribe({
         next: (data) => {
-          if (data) {
-            // navigation
-            console.log(data);
+          if(data){
             this.router.navigateByUrl('/profile');
           }
-        }, error: (err) => {
-          console.log(err);
         },
-      })
-    } else {
-      this.errorMessage = 'Form is empty.';
+        error: (err) =>{
+          console.error(err);
+        }
+      });
     }
   }
 
